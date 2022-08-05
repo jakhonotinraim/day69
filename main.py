e0a9bb017fbdab08007ae7f8dfa62b2382d7e15b
jakhonotinraim <107978729+jakhonotinraim@users.noreply.github.com>
2022-08-05 12:07:26 +0500

    Procfile

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ from sqlalchemy import exc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, Column, Integer, ForeignKey
 
-from 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
